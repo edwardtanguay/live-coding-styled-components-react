5dd35d78ec7898b26226f454559e49877449cbf7
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 09:45:58 +0200

    roll back

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ const url = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
 
 function App() {
 	const [nouns, setNouns] = useState([]);
-  const [choice, setChoice] = useState('hide');
+	const [choice, setChoice] = useState('hide');
 
 	useEffect(() => {
 		(async () => {
-      const _nouns = (await axios.get(url)).data;
-      qstr.randomize(_nouns);
+			const _nouns = (await axios.get(url)).data;
+			qstr.randomize(_nouns);
 			setNouns(_nouns);
 		})();
 	}, []);
@@ -20,19 +20,22 @@ function App() {
 	return (
 		<div className="App">
 			<h1>German Article Practice</h1>
-      <div className="buttons">
-        <button className={choice === 'der' ? 'active' : ''} onClick={() => setChoice('der')}>der</button>
-        <button onClick={() => setChoice('die')}>die</button>
-        <button onClick={() => setChoice('das')}>das</button>
-        <button onClick={() => setChoice('hide')}>hide</button>
-      </div>
-      <div className="nouns">
-        {nouns.map((noun, index) => {
-          return (
-            <div className="noun">{noun.article} {noun.singular}</div>
-          )
-        })}
-      </div>
+			<div className="buttons">
+				<button onClick={() => setChoice('der')}>der</button>
+				<button onClick={() => setChoice('die')}>die</button>
+				<button onClick={() => setChoice('die')}>das</button>
+				<button onClick={() => setChoice('hide')}>hide</button>
+			</div>
+
+			<div className="nouns">
+				{nouns.map((noun, index) => {
+					return (
+						<div className="noun">
+							{noun.article} {noun.singular}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
