5370a3600cb773d81d7b8beb1afb5434d9ccf234
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 09:55:42 +0200

    basic styled-component in

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 import axios from 'axios';
 import * as qstr from './qstr';
+import styled from 'styled-components';
 
 const url = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
 
 function App() {
 	const [nouns, setNouns] = useState([]);
 	const [choice, setChoice] = useState('hide');
+
+  const Button = styled.button`
+      background-color: green;
+  `;
 
 	useEffect(() => {
 		(async () => {
@@ -22,6 +27,7 @@ function App() {
 			<h1>German Article Practice</h1>
 			<div className="buttons">
 				<button onClick={() => setChoice('der')}>der</button>
+        <Button >der</Button>
 				<button onClick={() => setChoice('die')}>die</button>
 				<button onClick={() => setChoice('die')}>das</button>
 				<button onClick={() => setChoice('hide')}>hide</button>
